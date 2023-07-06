ee493ba9c73adaad1ec7204a5fd14760a40126fe
Markus Konkol <m.konkol@52north.org>
2023-07-06 16:52:39 +0200

    first version of metadata block without folding

---
+++ src/apps/onestop4all/components/ResourceType/Metadata/Metadata.tsx
@@ -0,0 +1,57 @@
+import { HStack } from "@open-pioneer/chakra-integration";
+
+export const Metadata = (props: { hide: boolean; metadataElements: object }) => {
+    const hide = props.hide;
+    const metadataElements = Object.values(props.metadataElements);
+
+    function isUrl(url: string) {
+        const urlRegex = /^(https?|ftp):\/\/[^\s/$.?#].[^\s]*$/i;
+        return urlRegex.test(url);
+    }
+
+    return (
+        <div hidden={hide} style={{ maxWidth: "50%", marginLeft: "10%" }}>
+            <p className="metaHeader">Metadata</p>
+            <div className="seperator"></div>
+            <>
+                {metadataElements.map((elem) =>
+                    elem.tag != "Keywords" ? (
+                        <>
+                            <div>
+                                {!isUrl(elem.val) ? (
+                                    <HStack>
+                                        <span className="tag">{elem.tag}: </span>
+                                        <span className="value">{elem.val}</span>
+                                    </HStack>
+                                ) : (
+                                    <HStack>
+                                        <span className="tag">{elem.tag}: </span>{" "}
+                                        <span className="value">
+                                            <a href={elem.val} className="link">
+                                                {elem.val}
+                                            </a>
+                                        </span>
+                                    </HStack>
+                                )}
+                            </div>
+                            <div className="seperator"></div>
+                        </>
+                    ) : (
+                        <>
+                            <div>
+                                <span className="tag">{elem.tag}: </span>
+                                {elem.val.map((e: string) => (
+                                    <>
+                                        <div className="keyword">
+                                            <a href={"/search?keyword=" + e}>{e}</a>
+                                        </div>
+                                    </>
+                                ))}
+                            </div>
+                        </>
+                    )
+                )}
+            </>
+        </div>
+    );
+};
--- src/apps/onestop4all/views/Standard.tsx
+++ src/apps/onestop4all/views/Standard.tsx
@@ -1,17 +1,58 @@
 import { Box, Container, Image, Grid, GridItem } from "@open-pioneer/chakra-integration";
 import { SearchBar } from "../components/SearchBar";
 import { ResourceTypeHeader } from "../components/ResourceType/ResourceTypeHeader";
+import { Metadata } from "../components/ResourceType/Metadata/Metadata";
 
 export function StandardView() {
+    const metadataElements = [
+        { tag: "Standards organization", val: "Open Geospatial Consortium" },
+        { tag: "Date of publication", val: "10.07.2014" },
+        { tag: "Version", val: "2.0.2" },
+        { tag: "External Identifier", val: "http://www.opengis.net/doc/IS/wfs/2.0.2" },
+        {
+            tag: "Keywords",
+            val: [
+                "ogcdoc",
+                "OGC document",
+                "web feature service",
+                "wfs",
+                "property",
+                "geographic information",
+                "resource",
+                "geography markup language",
+                "GML",
+                "Transaction",
+                "GetFeature",
+                "GetCapabilities",
+                "stored query",
+                "XML",
+                "KVP",
+                "encoding",
+                "Schema",
+                "HTTP",
+                "GET",
+                "POST",
+                "SOAP",
+                "request",
+                "response",
+                "capabilities document",
+                "filter encoding",
+                "contraint"
+            ]
+        }
+    ];
+    const hideMetadata = false;
     return (
         <>
             <Image src="/image2.png" width="100%" />
-            <Box position="absolute" width="100%" marginTop="-50px">BorderColor
+            <Box position="absolute" width="100%" marginTop="-50px">
+                BorderColor
                 <Container maxW="80%">
                     <SearchBar></SearchBar>
                 </Container>
             </Box>
             <ResourceTypeHeader resType="standard"></ResourceTypeHeader>
+            <Metadata hide={hideMetadata} metadataElements={metadataElements} />
         </>
     );
 }
