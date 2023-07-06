f746721f5dcfb89914f61f9c3e828e6dae3339f9
Jan Schulte <j.schulte@52north.org>
2023-07-06 13:05:49 +0200

    restructure start view

--- src/apps/onestop4all/views/Start/Start.tsx
+++ src/apps/onestop4all/views/Start/Start.tsx
@@ -1,7 +1,6 @@
 import { Box, Container, Flex, Image, Spacer } from "@open-pioneer/chakra-integration";
 
 import { SearchBar } from "../../components/SearchBar";
-import { Mission } from "./Mission/Mission";
 
 export function StartView() {
     return (
@@ -32,7 +31,23 @@ export function StartView() {
 
             <Container maxW="80%">
                 <Box height="80px"></Box>
-                <Mission></Mission>
+
+                <Box className="mission">
+                    <img className="bg-icon" alt="Bg icon" src="/bg-icon.png" />
+                    <Box className="mission-text text-centered-box">
+                        <Box className="text-centered-box-text">
+                            The <span className="bold">OneStop4All</span> is the primary visual and
+                            user-friendly <span className="link">NFDI4Earth</span> access point. It
+                            offers a coherent view on and points to all relevant Earth System
+                            Sciences (ESS) RDM resources provided by NFDI4Earth members and the ESS
+                            community, such as data repositories, software tools, information on
+                            Research Data Management (RDM), education and training materials. Learn
+                            more about our <span className="link">Mission</span> and the{" "}
+                            <span className="link">Resources</span>.
+                        </Box>
+                    </Box>
+                </Box>
+
                 <div className="seperator"></div>
             </Container>
         </>
