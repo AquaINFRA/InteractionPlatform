7541963ecd844a884f2c15dbda9ec30b72a4bf04
Markus Konkol <m.konkol@52north.org>
2023-07-12 15:13:32 +0200

    improve show/hide metadata

--- src/apps/onestop4all/components/ResourceType/Metadata/Metadata.tsx
+++ src/apps/onestop4all/components/ResourceType/Metadata/Metadata.tsx
@@ -8,90 +8,68 @@ import {
 } from "@open-pioneer/chakra-integration";
 import { useState } from "react";
 import { UpIcon, DownIcon } from "../../Icons";
+import { MetadataContent } from "./MetadataContent";
 
-export const Metadata = (props: { metadataElements: object }) => {
+export const Metadata = (props: { metadataElements: object; visibleElements: number }) => {
+    const visibleElements = props.visibleElements;
+    const [expanded, setExpanded] = useState(true);
     const metadataElements = Object.values(props.metadataElements);
 
-    function isUrl(url: string) {
-        const urlRegex = /^(https?|ftp):\/\/[^\s/$.?#].[^\s]*$/i;
-        return urlRegex.test(url);
-    }
-
-    const [expanded, setExpanded] = useState(true);
-
     return (
-        <Accordion allowMultiple defaultIndex={expanded ? [0] : [1]}>
-            <AccordionItem>
-                <AccordionPanel>
-                    <Box>
-                        <p className="metadataSectionHeader">Metadata</p>
-                        <div className="seperator"></div>
-                        {metadataElements.map((e, i) =>
-                            e.tag != "Keywords" ? (
-                                <Box key={i}>
-                                    <div>
-                                        {!isUrl(e.val) ? (
-                                            <Flex>
-                                                <span className="metadataTag">{e.tag}:&nbsp;</span>
-                                                <span className="metadataValue">{e.val}</span>
-                                            </Flex>
-                                        ) : (
-                                            <Flex>
-                                                <span className="metadataTag">{e.tag}:&nbsp;</span>{" "}
-                                                <span className="metadataValue">
-                                                    <a href={e.val} className="metadataLink">
-                                                        {e.val}
-                                                    </a>
-                                                </span>
-                                            </Flex>
-                                        )}
-                                    </div>
-                                    <div className="seperator"></div>
-                                </Box>
-                            ) : (
-                                <Box key={i}>
-                                    <span className="metadataTag">{e.tag}:&nbsp;</span>
-                                    {e.val.map((elem: string, j: number) => (
-                                        <a
-                                            href={"/search?keyword=" + elem}
-                                            className="metadataKeyword"
-                                            key={j}
-                                        >
-                                            {elem}
-                                        </a>
-                                    ))}
-                                </Box>
-                            )
-                        )}
-                    </Box>
-                </AccordionPanel>
-                <h2>
-                    <AccordionButton onClick={() => setExpanded(!expanded)}>
-                        <Flex
-                            alignItems="center"
-                            direction="column"
-                            gap="4px"
-                            style={{ marginLeft: "40%", marginRight: "40%" }}
-                        >
-                            {expanded ? (
-                                <>
-                                    <Box>
-                                        <UpIcon />
-                                    </Box>
-                                    <Box className="metadataShowHide">Hide metadata</Box>
-                                </>
-                            ) : (
-                                <>
-                                    <Box className="metadataShowHide">Show metadata</Box>
-                                    <Box>
-                                        <DownIcon />
-                                    </Box>
-                                </>
-                            )}
-                        </Flex>
-                    </AccordionButton>
-                </h2>
-            </AccordionItem>
-        </Accordion>
+        <Box>
+            <Box>
+                <p className="metadataSectionHeader">Metadata</p>
+                <MetadataContent
+                    metadataElements={props.metadataElements}
+                    start={0}
+                    end={visibleElements}
+                />
+            </Box>
+            <Accordion allowMultiple defaultIndex={expanded ? [0] : [1]}>
+                <AccordionItem borderTopWidth="0 !important" borderBottomWidth="0 !important">
+                    <AccordionPanel
+                        paddingInlineStart="unset !important"
+                        paddingInlineEnd="unset !important"
+                        paddingTop="unset !important"
+                    >
+                        <MetadataContent
+                            metadataElements={props.metadataElements}
+                            start={visibleElements}
+                            end={metadataElements.length}
+                        />
+                    </AccordionPanel>
+                    {visibleElements < metadataElements.length ? (
+                        <h2>
+                            <AccordionButton onClick={() => setExpanded(!expanded)}>
+                                <Flex
+                                    alignItems="center"
+                                    direction="column"
+                                    gap="4px"
+                                    style={{ marginLeft: "40%", marginRight: "40%" }}
+                                >
+                                    {expanded ? (
+                                        <>
+                                            <Box>
+                                                <UpIcon />
+                                            </Box>
+                                            <Box className="metadataShowHide">Hide metadata</Box>
+                                        </>
+                                    ) : (
+                                        <>
+                                            <Box className="metadataShowHide">Show full list</Box>
+                                            <Box>
+                                                <DownIcon />
+                                            </Box>
+                                        </>
+                                    )}
+                                </Flex>
+                            </AccordionButton>
+                        </h2>
+                    ) : (
+                        <></>
+                    )}
+                </AccordionItem>
+            </Accordion>
+        </Box>
     );
 };
--- src/apps/onestop4all/views/Standard/Standard.tsx
+++ src/apps/onestop4all/views/Standard/Standard.tsx
@@ -10,102 +10,81 @@ import { ExternalLinkIcon, LinkIcon } from "@chakra-ui/icons";
 import { PdfIcon, MetadataSourceIcon } from "../../components/Icons";
 
 export function StandardView() {
-    const metadataElements = [
-        { tag: "Standards organization", val: "Open Geospatial Consortium" },
-        { tag: "Date of publication", val: "10.07.2014" },
-        { tag: "Version", val: "2.0.2" },
-        { tag: "External Identifier", val: "http://www.opengis.net/doc/IS/wfs/2.0.2" },
-        {
-            tag: "Keywords",
-            val: [
-                "ogcdoc",
-                "OGC document",
-                "web feature service",
-                "wfs",
-                "property",
-                "geographic information",
-                "resource",
-                "geography markup language",
-                "GML",
-                "Transaction",
-                "GetFeature",
-                "GetCapabilities",
-                "stored query",
-                "XML",
-                "KVP",
-                "encoding",
-                "Schema",
-                "HTTP",
-                "GET",
-                "POST",
-                "SOAP",
-                "request",
-                "response",
-                "capabilities document",
-                "filter encoding",
-                "contraint"
-            ]
-        }
-    ];
-    const abstractText =
-        "The Web Feature Service (WFS) represents a change in the way geographic information is created, modified and exchanged on the Internet." +
-        "Rather than sharing geographic information at the file level using File Transfer Protocol (FTP), for example, the WFS offers direct fine-grained access to geographic information at the feature and feature property level." +
-        "This International Standard specifies discovery operations, query operations, locking operations, transaction operations and operations to manage stored, parameterized query expressions. " +
-        "Discovery operations allow the service to be interrogated to determine its capabilities and to retrieve the application schema that defines the feature types that the service offers. " +
-        "Query operations allow features or values of feature properties to be retrieved from the underlying data store based upon constraints, defined by the client, on feature properties. " +
-        "Locking operations allow exclusive access to features for the purpose of modifying or deleting features." +
-        "Transaction operations allow features to be created, changed, replaced and deleted from the underlying data store." +
-        "Stored query operations allow clients to create, drop, list and described parameterized query expressions that are stored by the server and can be repeatedly invoked using different parameter values." +
-        "This International Standard defines eleven operations:<br>" +
-        "GetCapabilities (discovery operation)" +
-        "- DescribeFeatureType (discovery operation)" +
-        "- GetPropertyValue (query operation)" +
-        "- GetFeature (query operation)" +
-        "- GetFeatureWithLock (query & locking operation)" +
-        "- LockFeature (locking operation)" +
-        "- Transaction (transaction operation)" +
-        "- CreateStoredQuery (stored query operation)" +
-        "- DropStoredQuery (stored query operation)" +
-        "- ListStoredQueries (stored query operation)" +
-        "- DescribeStoredQueries (stored query operation)" +
-        "In the taxonomy of services defined in ISO 19119, the WFS is primarily a feature access service but also includes elements of a feature type service, a coordinate conversion/transformation service and geographic format conversion service.";
+    const metadataResponse = {
+        title: "OGC Web Map Service",
+        abstract:
+            "The Web Feature Service (WFS) represents a change in the way geographic information is created, modified and exchanged on the Internet. Rather than sharing geographic information at the file level using File Transfer Protocol (FTP), for example, the WFS offers direct fine-grained access to geographic information at the feature and feature property level. This International Standard specifies discovery operations, query operations, locking operations, transaction operations and operations to manage stored, parameterized query expressions. Discovery operations allow the service to be interrogated to determine its capabilities and to retrieve the application schema that defines the feature types that the service offers. Query operations allow features or values of feature properties to be retrieved from the underlying data store based upon constraints, defined by the client, on feature properties. Locking operations allow exclusive access to features for the purpose of modifying or deleting features. Transaction operations allow features to be created, changed, replaced and deleted from the underlying data store. Stored query operations allow clients to create, drop, list and described parameterized query expressions that are stored by the server and can be repeatedly invoked using different parameter values. This International Standard defines eleven operations: GetCapabilities (discovery operation) DescribeFeatureType (discovery operation) GetPropertyValue (query operation) GetFeature (query operation) GetFeatureWithLock (query & locking operation) LockFeature (locking operation) - Transaction (transaction operation) CreateStoredQuery (stored query operation) DropStoredQuery (stored query operation) ListStoredQueries (stored query operation) DescribeStoredQueries (stored query operation) In the taxonomy of services defined in ISO 19119, the WFS is primarily a feature access service but also includes elements of a feature type service, a coordinate conversion/transformation service and geographic format conversion service.",
+        standardsOrganization: "Open Geospatial Consortium",
+        dateOfPublication: "10.07.2014",
+        version: "2.0.2",
+        externalIdentifier: "http://www.opengis.net/doc/IS/wfs/2.0.2",
+        keywords: [
+            "ogcdoc",
+            "OGC document",
+            "web feature service",
+            "wfs",
+            "property",
+            "geographic information",
+            "resource",
+            "geography markup language",
+            "GML",
+            "Transaction",
+            "GetFeature",
+            "GetCapabilities",
+            "stored query",
+            "XML",
+            "KVP",
+            "encoding",
+            "Schema",
+            "HTTP",
+            "GET",
+            "POST",
+            "SOAP",
+            "request",
+            "response",
+            "capabilities document",
+            "filter encoding",
+            "contraint"
+        ],
+        relatedContentItems: [
+            {
+                resourceType: "Repositories / Archives",
+                title: "Environmental Information Data Centre",
+                url: "https://www.nfdi4earth.de/"
+            },
+            {
+                resourceType: "Services",
+                title: "Environmental Information Data Centre",
+                url: "https://www.nfdi4earth.de/"
+            },
+            {
+                resourceType: "Educational resources",
+                title: "Environmental Information Data Centre",
+                url: "https://www.nfdi4earth.de/"
+            },
+            {
+                resourceType: "Documents",
+                title: "Environmental Information Data Centre",
+                url: "https://www.nfdi4earth.de/"
+            },
+            {
+                resourceType: "Documents",
+                title: "Environmental Information Data Centre",
+                url: "https://www.nfdi4earth.de/"
+            },
+            {
+                resourceType: "Documents",
+                title: "Environmental Information Data Centre",
+                url: "https://www.nfdi4earth.de/"
+            }
+        ]
+    };
+
     const hideMetadata = false;
-    const title = "OGC Web Map Service";
-    const relatedContentItems = [
-        {
-            resourceType: "Repositories / Archives",
-            title: "Environmental Information Data Centre",
-            url: "https://www.nfdi4earth.de/"
-        },
-        {
-            resourceType: "Services",
-            title: "Environmental Information Data Centre",
-            url: "https://www.nfdi4earth.de/"
-        },
-        {
-            resourceType: "Educational resources",
-            title: "Environmental Information Data Centre",
-            url: "https://www.nfdi4earth.de/"
-        },
-        {
-            resourceType: "Documents",
-            title: "Environmental Information Data Centre",
-            url: "https://www.nfdi4earth.de/"
-        },
-        {
-            resourceType: "Documents",
-            title: "Environmental Information Data Centre",
-            url: "https://www.nfdi4earth.de/"
-        },
-        {
-            resourceType: "Documents",
-            title: "Environmental Information Data Centre",
-            url: "https://www.nfdi4earth.de/"
-        }
-    ];
     const fun = () => {
         console.log("This is a fun");
     };
+
     return (
         <Box>
             <Box position="relative">
@@ -124,13 +103,31 @@ export function StandardView() {
                     <Box w="65%">
                         <ResourceTypeHeader resType="Standards" />
                         <Box className="title" pt="15px">
-                            {title}
+                            {metadataResponse["title"]}
                         </Box>
                         <Box hidden={hideMetadata} pt="36px">
-                            <Metadata metadataElements={metadataElements} />
+                            <Metadata
+                                metadataElements={[
+                                    {
+                                        tag: "Standards organization",
+                                        val: metadataResponse["standardsOrganization"]
+                                    },
+                                    {
+                                        tag: "Date of publication",
+                                        val: metadataResponse["dateOfPublication"]
+                                    },
+                                    { tag: "Version", val: metadataResponse["version"] },
+                                    {
+                                        tag: "External identifier",
+                                        val: metadataResponse["externalIdentifier"]
+                                    },
+                                    { tag: "Keywords", val: metadataResponse["keywords"] }
+                                ]}
+                                visibleElements={4}
+                            />
                         </Box>
                         <Box pt="80px">
-                            <Abstract abstractText={abstractText} />
+                            <Abstract abstractText={metadataResponse["abstract"]} />
                         </Box>
                     </Box>
                     <Box w="25%">
@@ -165,7 +162,9 @@ export function StandardView() {
                 </Flex>
                 <Box w="100%" pt="80px">
                     <Box>
-                        <RelatedContent relatedContentItems={relatedContentItems} />
+                        <RelatedContent
+                            relatedContentItems={metadataResponse["relatedContentItems"]}
+                        />
                     </Box>
                     <Flex gap="10%" alignItems="center" pt="120px">
                         <Divider className="seperator" w="65%" />
