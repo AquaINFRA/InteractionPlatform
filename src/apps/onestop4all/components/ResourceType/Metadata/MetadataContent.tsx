5a168af58c39cae5049b6073d7a1f43025027c65
Markus Konkol <m.konkol@52north.org>
2023-07-17 15:10:54 +0200

    code improvements

--- src/apps/onestop4all/components/ResourceType/Metadata/MetadataContent.tsx
+++ src/apps/onestop4all/components/ResourceType/Metadata/MetadataContent.tsx
@@ -1,6 +1,7 @@
-import { Flex, Box } from "@open-pioneer/chakra-integration";
+import { Box } from "@open-pioneer/chakra-integration";
 import { Keywords } from "./Keywords";
 import { Authors } from "./Authors";
+import { Misc } from "./Misc";
 
 export const MetadataContent = (props: {
     metadataElements: object;
@@ -10,41 +11,19 @@ export const MetadataContent = (props: {
     const { start, end } = props;
     const metadataElements = Object.values(props.metadataElements);
 
-    function isUrl(url: string) {
-        const urlRegex = /^(https?|ftp):\/\/[^\s/$.?#].[^\s]*$/i;
-        return urlRegex.test(url);
-    }
-
     return (
         <Box>
-            {metadataElements.slice(start, end).map((e, i) =>
-                e.tag == "Keywords" ? (
-                    <Keywords key={i} keywords={e.val} />
-                ) : e.tag == "Authors" ? (
-                    <Authors key={i} authors={e.val} />
-                ) : (
-                    <Box key={i}>
-                        <div className="seperator"></div>
-                        <div>
-                            {!isUrl(e.val) ? (
-                                <Flex>
-                                    <span className="metadataTag">{e.tag}:&nbsp;</span>
-                                    <span className="metadataValue">{e.val}</span>
-                                </Flex>
-                            ) : (
-                                <Flex>
-                                    <span className="metadataTag">{e.tag}:&nbsp;</span>{" "}
-                                    <span className="metadataValue">
-                                        <a href={e.val} className="metadataLink">
-                                            {e.val}
-                                        </a>
-                                    </span>
-                                </Flex>
-                            )}
-                        </div>
-                    </Box>
-                )
-            )}
+            {metadataElements
+                .slice(start, end)
+                .map((e, i) =>
+                    e.tag == "Keywords" ? (
+                        <Keywords key={i} keywords={e.val} />
+                    ) : e.tag == "Authors" ? (
+                        <Authors key={i} authors={e.val} />
+                    ) : (
+                        <Misc key={i} tag={e.tag} val={e.val} />
+                    )
+                )}
         </Box>
     );
 };
