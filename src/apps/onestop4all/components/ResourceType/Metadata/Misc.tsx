ae66635bc26cca93399ca8f504ace7125575de9e
Markus Konkol <m.konkol@52north.org>
2023-08-21 15:48:58 +0200

    identify email address and link accordingly

--- src/apps/onestop4all/components/ResourceType/Metadata/Misc.tsx
+++ src/apps/onestop4all/components/ResourceType/Metadata/Misc.tsx
@@ -3,15 +3,26 @@ import { Box } from "@open-pioneer/chakra-integration";
 export const Misc = (props: { tag: string; val: Array<string> }) => {
     const { tag, val } = props;
 
+    //To do: Create reusable function, used at several locations
     function isUrl(url: string) {
         const urlRegex = /^(https?|ftp):\/\/[^\s/$.?#].[^\s]*$/i;
         return urlRegex.test(url);
     }
 
-    const MetadataUrl = (props: { item: string }) => {
-        const item = props.item;
+    function isEmail(address: string) {
+        const emailRegex = /^[^@\s]+@[^@\s]+\.[^@\s]+$/;
+        return emailRegex.test(address);
+    }
+
+    const MetadataUrl = (props: { item: string; type: string }) => {
+        const { item, type } = props;
         return (
-            <a href={item} className="metadataLink" rel="noreferrer" target="_blank">
+            <a
+                href={type == "mail" ? "mailto: " : "" + item}
+                className="metadataLink"
+                rel="noreferrer"
+                target="_blank"
+            >
                 {item}
             </a>
         );
@@ -24,20 +35,31 @@ export const Misc = (props: { tag: string; val: Array<string> }) => {
             {typeof val == "object" ? (
                 val.map((elem: string, i: number) => (
                     <span className="metadataValue" key={i}>
-                        {!isUrl(elem) ? (
-                            elem + (i < val.length - 1 ? ";" : "")
-                        ) : (
+                        {isUrl(elem) ? (
                             <span>
-                                <MetadataUrl item={elem} />
+                                <MetadataUrl item={elem} type="url" />
                                 {i < val.length - 1 ? "; " : ""}
                             </span>
+                        ) : isEmail(elem) ? (
+                            <span>
+                                <MetadataUrl item={elem} type="mail" />
+                                {i < val.length - 1 ? "; " : ""}
+                            </span>
+                        ) : (
+                            elem + (i < val.length - 1 ? ";" : "")
                         )}
                         &nbsp;
                     </span>
                 ))
             ) : (
                 <span className="metadataValue">
-                    {!isUrl(val) ? val : <MetadataUrl item={val} />}
+                    {!isUrl(val) ? (
+                        val
+                    ) : isEmail(val) ? (
+                        <MetadataUrl item={val} type="mail" />
+                    ) : (
+                        <MetadataUrl item={val} type="url" />
+                    )}
                 </span>
             )}
         </Box>
