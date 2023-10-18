b98589bb44fb5908a5d082c6aee2545c41c34d85
Markus Konkol <m.konkol@52north.org>
2023-10-18 08:37:10 +0200

    add alt_name as an alternative if name is not in the metadata

--- src/apps/onestop4all/views/Result/Result.tsx
+++ src/apps/onestop4all/views/Result/Result.tsx
@@ -77,6 +77,7 @@ export function Result() {
                 setSearchResult(result.results[0]);
                 setResourceType(mapToResourceType(result.results[0].type));
                 console.log(result.results[0]);
+                console.log(result.results[0].homepage);
                 setLoading(false);
             } else {
                 // TODO: error handling
