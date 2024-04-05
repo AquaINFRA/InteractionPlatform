b6bb4719ba8dd84c8cc6e1d238abc932d56c8287
Jakob Bernd <j.bernd@52north.org>
2024-04-05 14:22:55 +0200

    clean up code

--- src/apps/onestop4all/views/Search/Facets/SpatialCoverageFacet/PopupOverlay.tsx
+++ src/apps/onestop4all/views/Search/Facets/SpatialCoverageFacet/PopupOverlay.tsx
@@ -31,6 +31,8 @@ import { Feature, MapBrowserEvent } from "ol";
 import Geometry from "ol/geom";
 import { Button } from "@open-pioneer/chakra-integration";
 import DragBox from "ol/interaction/DragBox";
+import Collection from "ol";
+import Interaction from "ol/interaction/Interaction";
 // Custom Control Buttons (not used currently)
 export class DrawControl extends Control {
     private handle: () => void;
@@ -54,24 +56,38 @@ interface PopupOverlayProps {
 }
 
 export function PopupOverlay({ showPopup, onClose }: PopupOverlayProps) {
-    console.log("Reloaded the page!");
+    console.log("Page reloaded!");
+    /********************************Initialization******************************* */
     // Initialize the map
     const mapId = "popup";
     const { map } = useMap(mapId);
     const source = new VectorSource();
     const olMapRegistry = useService("ol-map.MapRegistry");
-    const [renderState, setRenderState] = useState(false);
+
+    // inititalize state
+    const [renderState, setRenderState] = useState(false); //hook 11
     const draw = useRef<Draw>();
 
     const [vectorLayer, setVectorLayer] = useState(new VectorLayer({ source: source }));
     const [bBoxVectorLayer, setBBoxVectorLayer] = useState(new VectorLayer());
 
-    const [areFeaturesSelected, setAreFeaturesSelected] = useState(false);
-    const [selectedAreas, setSelectedAreas] = useState<any>([]);
+    const [areFeaturesSelected, setAreFeaturesSelected] = useState(false); //hook 15
     const [isBBoxDisplayed, setisBBoxDisplayed] = useState(false);
 
     const searchState = useSearchState();
     const [bBox, setBBox] = useState<Feature<any>[]>();
+
+    const [drawing, setDrawing] = useState(false);
+
+    console.log(
+        "Current states: drawing = " +
+            drawing +
+            ", areFeaturesSelected = " +
+            areFeaturesSelected +
+            ", isBBoxDisplayed =" +
+            isBBoxDisplayed +
+            ", selectedAreas = "
+    );
 
     // Center the map in Europe everytime the Popup gets opened
     useEffect(() => {
@@ -116,10 +132,12 @@ export function PopupOverlay({ showPopup, onClose }: PopupOverlayProps) {
         };
     }, [map]);
 
-    // Computes BBox containing all selected areas
-    function getBBox() {
-        const layers = map?.getAllLayers();
+    /**************************Feature:getBBox & setSearchArea**************** */
 
+    // Shows BBox containing all selected areas
+    function getBBox() {
+        // 1. Remove all but the first two layers
+        const layers = map?.getAllLayers();
         if (map && layers && layers.length > 2) {
             layers.forEach((layer, i) => {
                 if (i === 0 || i === 1) {
@@ -131,20 +149,19 @@ export function PopupOverlay({ showPopup, onClose }: PopupOverlayProps) {
                 }
             });
         }
-
-        let extentArrays = [] as any;
-        selectedAreas.forEach((area: any) => {
-            extentArrays = extentArrays.concat(area.geometry.extent_);
+        // 2. Compute BBox
+        let extentArrays = [] as number[];
+        const selectedFeatures = selectClick.getFeatures().getArray();
+        selectedFeatures.forEach((area: any) => {
+            extentArrays = extentArrays.concat(area.getGeometry().getExtent());
         });
 
         const xCoordinates = extentArrays.filter((_: any, index: any) => index % 2 === 0); // Even indices
         const yCoordinates = extentArrays.filter((_: any, index: any) => index % 2 !== 0); // Odd indices
-
         const minX = Math.min(...xCoordinates);
         const maxX = Math.max(...xCoordinates);
         const minY = Math.min(...yCoordinates);
         const maxY = Math.max(...yCoordinates);
-
         const bbox = [
             [minX, minY],
             [minX, maxY],
@@ -152,9 +169,8 @@ export function PopupOverlay({ showPopup, onClose }: PopupOverlayProps) {
             [maxX, minY],
             [minX, minY]
         ];
-
         const coordinates = bbox;
-
+        // 3. Display BBox
         const geojson = {
             type: "Feature",
             properties: {},
@@ -163,7 +179,6 @@ export function PopupOverlay({ showPopup, onClose }: PopupOverlayProps) {
                 coordinates: [coordinates] // Wrap coordinates in an array to represent a polygon
             }
         };
-
         const geoJSONFormat = new GeoJSON();
         const features = geoJSONFormat.readFeatures(geojson, {
             featureProjection: "EPSG:4326"
@@ -171,61 +186,17 @@ export function PopupOverlay({ showPopup, onClose }: PopupOverlayProps) {
         const vectorSourceBBox = new VectorSource({
             features: features
         });
-
         const vectorLayerBBox = new VectorLayer({
             source: vectorSourceBBox,
             style: bBoxStyle
         });
+        // Update map and state
         map?.addLayer(vectorLayerBBox);
         setBBoxVectorLayer(vectorLayerBBox);
         setisBBoxDisplayed(true);
         setBBox(features);
     }
-
-    // Re-Renders Map if you re-open the popup
-    function changeRenderState() {
-        if (renderState == true) setRenderState(false);
-        else setRenderState(true);
-    }
-    useEffect(() => {
-        if (showPopup) {
-            changeRenderState();
-        }
-    }, [showPopup]);
-
-    useEffect(() => {
-        const fetchMap = async () => {
-            try {
-                const map = await olMapRegistry.getMap(mapId);
-                map.render();
-            } catch (error) {
-                console.error("Fehler beim Abrufen der Karte:", error);
-            }
-        };
-        fetchMap();
-    }, [changeRenderState]);
-
-    // Clean up the map after closing
-    useEffect(() => {
-        if (!showPopup) {
-            removeInteraction();
-            // map?.removeLayer(bBoxVectorLayer);
-            // clearSelectedFeatures();
-        }
-    }, [showPopup]);
-
-    function removeInteraction() {
-        if (draw.current) {
-            map?.removeInteraction(draw.current);
-        }
-    }
-
-    function handleClose(): void {
-        onClose();
-        source.clear(); // clears the map
-        removeInteraction();
-    }
-
+    // HANDLER: Starts a new Search with the spatial filter given by the BBox
     function setSearchArea(): void {
         const features = bBox;
         if (features) {
@@ -243,31 +214,71 @@ export function PopupOverlay({ showPopup, onClose }: PopupOverlayProps) {
         }
     }
 
+    /***********************Fix bug where map doesnt render*************************************************** */
+
+    // Re-Renders Map if you re-open the popup
+    function toggleRenderState() {
+        if (renderState == true) setRenderState(false);
+        else setRenderState(true);
+    }
+    useEffect(() => {
+        if (showPopup) {
+            toggleRenderState();
+        } else {
+            removeDraw(); // clean up map after closing
+        }
+    }, [showPopup]);
+
+    useEffect(() => {
+        const fetchMap = async () => {
+            try {
+                const map = await olMapRegistry.getMap(mapId);
+                map.render();
+            } catch (error) {
+                console.error("Fehler beim Abrufen der Karte:", error);
+            }
+        };
+        fetchMap();
+    }, [renderState]);
+
+    /*********************************Various Buttons********************************* */
+    function removeDraw() {
+        if (draw.current) {
+            map?.removeInteraction(draw.current);
+        }
+    }
+    // HANDLER: Close the map
+    function handleClose(): void {
+        onClose();
+        source.clear(); // clears the map
+        removeDraw();
+    }
+
+    // HANDLER: Deselects all selectClick and selectHover (hover doesnt work yet)
     function deselectAll(): void {
         const selected = selectClick.getFeatures();
         if (selected.getLength() > 0) selected.clear();
         const selectedHover = selectHover.getFeatures();
         if (selectedHover.getLength() > 0) selectedHover.clear();
-
         map?.removeLayer(bBoxVectorLayer);
         setBBoxVectorLayer(new VectorLayer());
         setisBBoxDisplayed(false);
         setAreFeaturesSelected(false);
     }
     /***********************************Selectable Catchment areas ********************/
+    // testing stuff
+    const falsef = (event: any) => false;
+    let condition = pointerMove;
+    if (drawing) condition = falsef;
+
     // SelectHover
-    let selectedFeatures: any[] = [];
     const selectHover = new Select({
-        condition: pointerMove,
+        condition: condition,
         style: hoverStyle,
         toggleCondition: function (event) {
-            const isSelected = selectedFeatures.length > 0;
-            return !isSelected;
+            return false;
         },
         layers: [vectorLayer]
-    });
-    selectHover.on("select", function (event) {
-        selectedFeatures = event.target.getFeatures().getArray();
     });
 
     // SelectClick
@@ -277,26 +288,21 @@ export function PopupOverlay({ showPopup, onClose }: PopupOverlayProps) {
             style: selectStyle
         })
     );
+    /************************Feature: Draw a box to select multiple areas************************** */
+    // HANDLER: draw a box
     function handleSelectBox() {
-        return null;
+        toggleDrawing();
+    }
+    function toggleDrawing(): void {
+        if (drawing) setDrawing(false);
+        else setDrawing(true);
     }
 
     useEffect(() => {
         if (map && showPopup) {
             map.addInteraction(selectHover);
             map.addInteraction(selectClick);
-            selectClick.on("select", (e) => {
-                setAreFeaturesSelected(true);
-                const tmp = [] as any;
-                e.target
-                    .getFeatures()
-                    .getArray()
-                    .forEach((area: any) => {
-                        tmp.push(area.values_);
-                    });
-                //selectedAreas.push(selectedArea);
-                setSelectedAreas(tmp);
-            });
+            selectClick.on("select", () => setAreFeaturesSelected(true));
         } else {
             const selected = selectClick.getFeatures();
             if (selected.getLength() > 0) selected.remove(selected.item(0));
