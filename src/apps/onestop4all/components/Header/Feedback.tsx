11d2dd88b11b0399fa7d436a48f5963bc5fb73b5
MarkusKonk <m.konkol@52north.org>
2024-04-09 08:59:22 +0200

    add feedback doc

---
+++ src/apps/onestop4all/components/Header/Feedback.tsx
@@ -0,0 +1,15 @@
+import { Box } from "@open-pioneer/chakra-integration";
+
+export const Feedback = (props: { fontSize: string }) => {
+
+    return (
+        <Box w="100%">
+            <div style={{ textAlign: "center", fontSize: props.fontSize }}>
+                This platform is a beta version. Do you have feedback? 
+                Tell us <a href="https://docs.google.com/document/d/1GPDQSZjHOXkzKJW1q3k4GshlPcFaK4t5OpvWzXnUdpg/edit?usp=sharing" className="link" target="_blank" rel="noreferrer">
+                    <span style={{ textDecoration: "underline" }}>here</span>!
+                </a>
+            </div>
+        </Box>
+    );
+};
--- src/apps/onestop4all/components/Header/Header.tsx
+++ src/apps/onestop4all/components/Header/Header.tsx
@@ -1,11 +1,11 @@
-import { Box, Hide, HStack } from "@open-pioneer/chakra-integration";
+import { Box, Flex, Hide, HStack } from "@open-pioneer/chakra-integration";
 import { useNavigate } from "react-router-dom";
 import { useIntl } from "open-pioneer:react-hooks";
 
 import { BorderColor } from "../../Theme";
-import { Login } from "./Login";
 import { Logo } from "./Logo";
 import { MenuButton } from "./MenuButton";
+import { Feedback } from "./Feedback";
 
 export const Header = () => {
     const intl = useIntl();
@@ -16,21 +16,17 @@ export const Header = () => {
     };
 
     return (
-        <HStack
-            gap="20px"
-            margin="6px 0px"
-            padding="36px 0px"
-            borderBottom="1px solid"
-            borderBottomColor={BorderColor}
-        >
+        <Flex alignItems="center" justifyContent="space-between" margin="6px 0px" padding="36px 0px" borderBottom="1px solid" borderBottomColor={BorderColor}>
             <Box _hover={{ cursor: "pointer" }} onClick={backToStart}>
                 <Logo />
             </Box>
-            <Box w="100%"></Box>
-            <Hide below="custombreak">
-                {/*<Login />*/}
-            </Hide>
-            <MenuButton />
-        </HStack>
+            <Flex alignItems="center">
+                <Box id="feedback1" marginRight="50px">
+                    <Feedback fontSize="16pt" />
+                </Box>
+                <Hide below="custombreak" />
+                <MenuButton />
+            </Flex>
+        </Flex>
     );
 };
