2ce0fc96a0b33cce328a2e290f8e4dc89c365cb1
MarkusKonk <m.konkol@52north.org>
2026-02-09 13:30:05 +0100

    abstract formatting plus clickable urls

--- src/apps/onestop4all/components/ResourceType/Abstract/Abstract.tsx
+++ src/apps/onestop4all/components/ResourceType/Abstract/Abstract.tsx
@@ -1,14 +1,20 @@
 import { Box } from "@open-pioneer/chakra-integration";
+import parse from "html-react-parser";
 import { LinkItUrl } from "react-linkify-it";
 
-export const Abstract = (props: { abstractText: string }) => {
-    const { abstractText } = props;
+export const Abstract = ({ abstractText }: { abstractText: string }) => {
     return (
         <Box>
             <div className="abstractSectionHeader">Abstract</div>
-            <LinkItUrl>
-                <Box className="abstractText" dangerouslySetInnerHTML={{__html: abstractText}} />
-            </LinkItUrl>
+            <Box className="abstractText">
+                {parse(abstractText, {
+                    replace: (domNode: any) => {
+                        if (domNode.type === "text") {
+                            return <LinkItUrl>{domNode.data}</LinkItUrl>;
+                        }
+                    }
+                })}
+            </Box>
         </Box>
     );
 };
