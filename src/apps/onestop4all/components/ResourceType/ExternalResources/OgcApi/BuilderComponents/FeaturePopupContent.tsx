7649f4c70706df9139fd69530f05c6968d5e9e5e
MarkusKonk <m.konkol@52north.org>
2026-03-06 15:23:45 +0100

    avoid to long feature info window

--- src/apps/onestop4all/components/ResourceType/ExternalResources/OgcApi/BuilderComponents/FeaturePopupContent.tsx
+++ src/apps/onestop4all/components/ResourceType/ExternalResources/OgcApi/BuilderComponents/FeaturePopupContent.tsx
@@ -25,24 +25,25 @@ export const FeaturePopupContent = ({ properties, onClick, showButton }: Feature
                 <Text fontWeight="bold" fontSize="sm">
                     Feature Info
                 </Text>
-                {showButton && (
-                    <Tooltip label="Use extent of this catchment as a search bounding box" placement="top" hasArrow>
-                        <Button
-                            colorScheme="blue"
-                            onClick={onClick}
-                            borderRadius="full"
-                            p={2}
-                            display="flex"
-                            alignItems="center"
-                            justifyContent="center"
-                        >
-                            <SearchIcon boxSize={4} />
-                        </Button>
-                    </Tooltip>
-                )}
+                {
+                    showButton &&
+                        <Tooltip label="Use extent of this catchment as a search bounding box" placement="top" hasArrow>
+                            <Button
+                                colorScheme="blue"
+                                onClick={onClick}
+                                borderRadius="full"
+                                p={2}
+                                display="flex"
+                                alignItems="center"
+                                justifyContent="center"
+                            >
+                                <SearchIcon boxSize={4} />
+                            </Button>
+                        </Tooltip>
+                }
             </Box>
 
-            <Box display="flex" flexDirection="column" gap={1}>
+            <Box display="flex" flexDirection="column" gap={1} maxH={"230px"} overflow={"scroll"}>
                 {Object.entries(properties)
                     .filter(([k]) => k !== "geometry")
                     .map(([k, v]) => (
